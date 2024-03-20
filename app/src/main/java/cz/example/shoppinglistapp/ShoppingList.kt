86a0215291207cb1728c1e191e7a651870cbc2b7
kalipero <kalimil.vavricka@gmail.com>
2024-03-20 07:09:34 +0100

    button, popup alert, displaying added item with icons edit/delete

--- app/src/main/java/cz/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/cz/example/shoppinglistapp/ShoppingList.kt
@@ -1,14 +1,27 @@
 package cz.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +30,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,14 +46,16 @@ data class ShoppingItem(val id: Int,
 fun ShoppingListApp(){
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
 
-
+    // Button add item
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = {},
+            onClick = {showDialog = true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add item")
@@ -49,14 +66,109 @@ fun ShoppingListApp(){
                 .padding(16.dp)
         ){
             items(sItems){
-
+            shoppingListItem(item = it, {},{})
             }
         }
     }
 
+    // popup alert
     if(showDialog){
-        AlertDialog(onDismissRequest = { showDialog=false}) {
-            Text(text = "I aM ALERT")
-        }
+        AlertDialog(onDismissRequest = {showDialog=false},
+            confirmButton = {
+                    // ROW WITH BUTTONS
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                        // BUTTON ADD
+                        Button(onClick = {
+                            if(itemName.isNotBlank()){
+                                val newItem = ShoppingItem(
+                                    id = sItems.size+1,
+                                    name = itemName,
+                                    quantity = itemQuantity.toInt()
+                                )
+                                sItems += newItem
+                                showDialog = false
+                                itemName = ""
+                            }
+
+                        }) {
+                            Text(text = "Add")
+                        }
+                        // BUTTON CANCEL
+                        Button(onClick = {showDialog=false}) {
+                            Text(text = "Cancel")
+                        }
+                    }
+
+            },
+            title = { Text(text = "Add shopping item") },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = {itemName = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = {itemQuantity = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            }
+        )
+
     }
 }
+
+@Composable
+fun shoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row (
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color.Black),
+                shape = RoundedCornerShape(20),
+            ),
+            horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Row {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = "Upravit")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    Icons.Filled.Delete,
+                    contentDescription = "Smazat",
+                    tint = Color.Red)
+            }
+        }
+
+
+    }
+}
+
+//Icon(
+//                Icons.Filled.Delete,
+//                contentDescription = "Smazat",
+//                tint = Color.Red, // Nastaví barvu ikony na červenou
+//
+//            )
